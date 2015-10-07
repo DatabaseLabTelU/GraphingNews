726e67788a1190318201b12d6d32166a0d2e9f45
Feber <febrian.setianto@gmail.com>
2015-10-07 12:06:13 +0700

    change news extractor and news miner

--- src/graph/database/NewsExtractor.java
+++ src/graph/database/NewsExtractor.java
@@ -1,6 +1,9 @@
 package graph.database;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Jsoup;
@@ -10,6 +13,34 @@ import org.jsoup.nodes.Element;
 public class NewsExtractor {
 
     public static void main(String[] args) {
+        System.out.println(readFolder("C:\\Users\\Febrian\\Downloads\\Documents\\merdeka500").size());
+    }
+
+    /**
+     * mengolah folder root menjadi nodes. masukkan folder tanpa slash(/) atau
+     * back-slash(\) di akhir. contoh C:\Users\merdeka500
+     *
+     * @param uri
+     * @return nodes
+     */
+    public static List<Node> readFolder(String uri) {
+        List<Node> nodes = new ArrayList<>();
+        try {
+            File folder = new File(uri);
+            for (String folderName : folder.list()) {
+                String fileName = uri + "\\" + folderName + "\\html" + folderName + ".txt";
+                File file = new File(fileName);
+                Document doc = Jsoup.parse(file, "utf-8");
+                NewsMiner miner = new NewsMiner(doc);
+                nodes.add(miner.getAsNode());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return nodes;
+    }
+
+    private static void doSomething() {
         // 1 minute timeout
         int timeout = 60 * 1000;
 
--- src/graph/database/NewsMiner.java
+++ src/graph/database/NewsMiner.java
@@ -1,30 +1,42 @@
 package graph.database;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.TokenNameFinderModel;
-import opennlp.tools.util.Span;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * class ini bertanggung jawab untuk proses mining dari text
  */
 public class NewsMiner {
 
+    private Document document;
+
+    public NewsMiner(Document document) {
+        this.document = document;
+    }
+
+    /**
+     * method yang merubah atribut yang diberikan menjadi node
+     *
+     * @return node
+     */
+    public Node getAsNode() {
+        Node n = new Node();
+        n.addProperty("author", new Property(getAuthor(), DataType.String));
+        n.addProperty("date", new Property(getDate(), DataType.String));
+        n.addProperty("title", new Property(getTitle(), DataType.String));
+        n.addProperty("article", new Property(getArticle(), DataType.String));
+        return n;
+    }
+
     /**
      * method yang digunakan untuk menemukan title yang telah terdefinisi dari
      * variable text <br/>IS : variable text sudah terdefinisi <br/>FS : return
      * title yang ditemukan jika ditemukan atau null jika tidak
      *
-     * @param text
      * @return judul berita
      */
-    public String getTitle(String text) {
-        String title = null;
-        return title;
+    public String getTitle() {
+        return document.title().split("\\|")[0];
     }
 
     /**
@@ -32,32 +44,15 @@ public class NewsMiner {
      * variable text <br/>IS : variable text sudah terdefinisi <br/>FS : return
      * author yang ditemukan jika ditemukan atau null jika tidak
      *
-     * @param text
      * @return author dari berita
      */
-    public String getAuthor(String text) {
-        InputStream modelIn;
-        try {
-            modelIn = new FileInputStream("en-ner-person.bin");
-            TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
-            NameFinderME nameFinder = new NameFinderME(model);
-            modelIn.close();
-
-            String[] sentence = new String[]{
-                "Adam", "Levine", "is", "a", "singer"
-            };
-
-            Span[] nameSpans = nameFinder.find(sentence);
-            for (Span s : nameSpans) {
-                System.out.println(s.toString());
-            }
-            nameFinder.clearAdaptiveData();
-        } catch (IOException ex) {
-            Logger.getLogger(NewsMiner.class.getName()).log(Level.SEVERE, null, ex);
+    public String getAuthor() {
+        Element author = document.select("div#mdk-body-news-reporter").first();
+        if (author == null || !author.text().contains("|")) {
+            return null;
+        } else {
+            return author.text().split("\\|")[0].split(":")[1].trim();
         }
-
-        String author = null;
-        return author;
     }
 
     /**
@@ -65,12 +60,15 @@ public class NewsMiner {
      * variable text <br/>IS : variable text sudah terdefinisi <br/>FS : return
      * author yang ditemukan jika ditemukan atau null jika tidak
      *
-     * @param text
      * @return tanggal publish berita
      */
-    public String getDate(String text) {
-        String date = null;
-        return date;
+    public String getDate() {
+        Element tanggal = document.select("div#mdk-body-news-reporter").first();
+        if (tanggal == null || !tanggal.text().contains("|")) {
+            return null;
+        } else {
+            return tanggal.text().split("\\|")[1].trim();
+        }
     }
 
     /**
@@ -78,11 +76,10 @@ public class NewsMiner {
      * variable text <br/>IS : variable text sudah terdefinisi <br/>FS : return
      * author yang ditemukan jika ditemukan atau null jika tidak
      *
-     * @param text
      * @return artikel dalam berita
      */
-    public String getArticle(String text) {
-        String article = null;
-        return article;
+    public String getArticle() {
+        Element article = document.select("div#mdk-body-newsarea").first();
+        return article == null ? null : article.text();
     }
 }
