31f0a547e5b9073c8a9e023a60096c5d9b592ef5
Isjhar Kautsar <isjhar@gmail.com>
2015-10-31 12:02:15 +0700

    fix some error after merge feber news extractor

--- src/graph/database/Reader.java
+++ src/graph/database/Reader.java
@@ -6,6 +6,8 @@ package graph.database;
 
 import java.awt.image.BufferedImage;
 import java.util.List;
+import graph.database.*;
+import graph.database.extractor.NewsMiner;
 
 /**
  *
@@ -41,54 +43,7 @@ public abstract class Reader {
      */
     public Node convertDocToNode(String pathFile){
         // read file
-        readFile(pathFile);
-
-        // create text manager 
-        NewsMiner tm = new NewsMiner();
-
-        // get title
-        String judul = tm.getTitle(text);
-
-        // get author
-        String author = tm.getAuthor(text);
-
-        // get date
-        String date = tm.getTitle(text);
-
-        // get article
-        String article = tm.getArticle(text);
-
-        // ini node
-        Node node =  new Node();
-
-        // cek jika judulnya tidak ada
-        if(judul != null)
-            // add judul
-            node.addProperty("judul", new Property(judul, DataType.String));
-
-        // cek jika authornya tidak ada
-        if(author != null)
-            // add author
-            node.addProperty("author", new Property(author, DataType.String));
-
-        // cek jika datenya tidak ada
-        if(date != null)
-            // add date
-            node.addProperty("date", new Property(author, DataType.Date));
-
-        // cek jika articlenya tidak ada
-        if(article != null)
-            // add article
-            node.addProperty("article", new Property(author, DataType.String));
-
-        // cek jika image tidak ada
-        if(listImage != null)
-            for(int i = 0;i < listImage.size();i++)
-                // add image
-                node.addProperty("image" + (i + 1), new Property(listImage.get(i), DataType.Image));
-
-        return node;
-        
+        return null;
     }
     
 }
